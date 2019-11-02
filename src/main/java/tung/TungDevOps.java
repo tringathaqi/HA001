6156e65c2910f3905fe812748721030064414eb7
tringa <tringasg4@gmail.com>
2019-11-02 14:47:42 +0000

    Permiresimi i fajllit TungDevOps

--- src/main/java/tung/TungDevOps.java
+++ src/main/java/tung/TungDevOps.java
@@ -1,7 +1,14 @@
+
 package tung;
+import org.joda.time.LocalTime;
 
 public class TungDevOps {
+
 	public static void main(String[] args) {
+
+	LocalTime kohaAktuale = new LocalTime();
+	System.out.println("Koha aktuale eshte: " + kohaAktuale);
+
  	Pershendetje pershendet = new Pershendetje();
 	System.out.println(pershendet.thuajTung());
 	}
